7efdd5c13e35a38ac5855b9412b3349a42c0508e
Marian <shcherbatiuk.m17@gmail.com>
2023-06-13 20:38:11 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,22 @@ var searchButton = $("#searchButton");
 var breedNameElement = $("#breedName");
 var dogImageElement = $("#dogImage");
 var breedInfoElement = $("#breedInfo");
-// document.addEventListener("DOMContentLoaded", function () {
-//   getAllDogInfo();
-// });
 
-function displayDogData(data){
+function displayDogData(data, dogImage) {
   var breedInfo;
-      if (data.length > 0) {
-        breedInfo = `Life Span: ${data[0].life_span}\nTemperament: ${data[0].temperament}`;
-      } else {
-        breedInfo = "Breed information not found.";
-      }
-      console.log(breedInfo);
-      dogImageElement.attr("src", dogImage);
-      breedNameElement.text(breedName);
-      breedInfoElement.text(breedInfo);
+  if (data.length > 0) {
+    breedInfo = `Life Span: ${data[0].life_span}\nTemperament: ${data[0].temperament}`;
+  } else {
+    breedInfo = "Breed information not found.";
+  }
+  console.log(breedInfo);
+  console.log(dogImage);
+  dogImageElement.attr("src", dogImage[0]);
+  breedNameElement.text(breedNameInput.val());
+  breedInfoElement.text(breedInfo);
 }
 
-function getBreedDescription(breedName) {
+function getBreedDescription(breedName, dogImage) {
   fetch("https://api.thedogapi.com/v1/breeds/search?q=" + breedName, {
     headers: {
       "x-api-key":
@@ -29,26 +27,26 @@ function getBreedDescription(breedName) {
   })
     .then((response) => response.json())
     .then((data) => {
-      //display dog data
-      displayDogData(data)
+      displayDogData(data, dogImage);
     })
     .catch((error) => {
       console.error(error);
     });
 }
+
 function getDogImg(breedName) {
-  fetch(`https://dog.ceo/api/breed/${breedName}/images/random`)
+  fetch(`https://dog.ceo/api/breed/${breedName}/images`)
     .then((response) => response.json())
     .then((data) => {
-      var dogImage = data.message;
+      var dogImage = data.message.slice(0, 5);
       console.log(dogImage);
-      // get breed description
-      getBreedDescription(breedName)
+      getBreedDescription(breedName, dogImage);
     })
     .catch((error) => {
       console.error(error);
     });
 }
+
 function getAllDogInfo(breedName) {
   fetch("https://dog.ceo/api/breeds/list/all")
     .then((response) => response.json())
@@ -57,16 +55,13 @@ function getAllDogInfo(breedName) {
       console.log(breedNames);
       console.log(breedName);
       breedName = breedName.split("-");
-      console.log(breedName)
-        if (breedNames.includes(breedName[0])) {
-          // turning breedName back into string
-          breedName = breedName.join("/"); 
-          //get dog picture by breed
-          getDogImg(breedName)
-        } else {
-          alert("Breed not found.");
-        }
-      
+      console.log(breedName);
+      if (breedNames.includes(breedName[0])) {
+        breedName = breedName.join("/");
+        getDogImg(breedName);
+      } else {
+        alert("Breed not found.");
+      }
     })
     .catch((error) => {
       console.error(error);
@@ -75,12 +70,10 @@ function getAllDogInfo(breedName) {
 
 function handleBreedSearch(event) {
   event.preventDefault();
-  var breedName = breedNameInput.val().toLowerCase()
-  // .replace(" ", "-");
-  breedName = breedName.split(" ")
-  breedName = breedName.reverse()
-  breedName = breedName.join("-");
+  var breedName = breedNameInput.val().toLowerCase();
+  breedName = breedName.split(" ").reverse().join("-");
   console.log(breedName);
   getAllDogInfo(breedName);
 }
+
 searchButton.on("click", handleBreedSearch);
